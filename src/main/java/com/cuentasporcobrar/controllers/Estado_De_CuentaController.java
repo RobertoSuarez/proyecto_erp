375dbf0fed0efebed25b9a90fb73b56b4aa87cd6
AlexVega2001 <87383623+AlexVega2001@users.noreply.github.com>
2021-08-23 18:31:28 -0500

    Mejoramiento en la parte de reporte de estado de cuenta y modificación en los botones que se vean de forma circular.

--- src/main/java/com/cuentasporcobrar/controllers/Estado_De_CuentaController.java
+++ src/main/java/com/cuentasporcobrar/controllers/Estado_De_CuentaController.java
@@ -3,21 +3,25 @@ package com.cuentasporcobrar.controllers;
 
 import com.cuentasporcobrar.daos.Estado_De_CuentaDAO;
 import com.cuentasporcobrar.models.Estado_De_Cuenta;
+import java.io.File;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.primefaces.component.export.ExcelOptions;
 import org.primefaces.component.export.PDFOptions;
 
 @Named(value = "estado_De_CuentaController")
 @ViewScoped
 public class Estado_De_CuentaController implements Serializable {
-
-    //Componentes para tener estilos en las exportaciones
-    ExcelOptions excelOpt;
-    PDFOptions pdfOpt;
     
     //Se Declaran las clases Estado_De_Cuenta y Estado_De_CuentaDAO
     Estado_De_Cuenta estado_De_Cuenta;
@@ -40,30 +44,55 @@ public class Estado_De_CuentaController implements Serializable {
         estado_De_CuentaDAO =new Estado_De_CuentaDAO();
         lista_Estado_De_Cuenta=new ArrayList<>();
         lista_Estado_De_Cuenta=estado_De_CuentaDAO.obtenerTodosLosEstadosCuenta();
-        Design();
         } catch (Exception ex) {
             System.out.println("Error: " + ex.getMessage());
-        }
-        
+        }        
     }
     
-    public void Design() {
-        excelOpt = new ExcelOptions();
-        excelOpt.setFacetBgColor("#2E8BE4");
-        excelOpt.setFacetFontSize("12");
-        excelOpt.setFacetFontColor("#FFFFFF");
-        excelOpt.setFacetFontStyle("BOLD");
-        excelOpt.setCellFontSize("11");
-        excelOpt.setAutoSizeColumn(true);
-        excelOpt.setFontName("Roboto");
+    public void exportarPDF() {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        ExternalContext ec = fc.getExternalContext();
 
-        pdfOpt = new PDFOptions();
-        pdfOpt.setFacetBgColor("#2E8BE4");
-        pdfOpt.setFacetFontSize("14");
-        pdfOpt.setFacetFontColor("#FFFFFF");
-        pdfOpt.setFacetFontStyle("BOLD");
-        pdfOpt.setCellFontSize("12");
-        pdfOpt.setFontName("Roboto");
+        // Cabecera de la respuesta.
+        ec.responseReset();
+        ec.setResponseContentType("application/pdf");
+        ec.setResponseHeader("Content-disposition", "attachment; filename=ReporteEstadoDeCuenta.pdf");
+
+        // tomamos el stream para llenarlo con el pdf.
+        try (OutputStream stream = ec.getResponseOutputStream()) {
+
+            // Parametros para el reporte.
+//            Map<String, Object> parametros = new HashMap<String, Object>();
+//            parametros.put("titulo", "Reporte desde java");
+//            parametros.put("fecha", LocalDate.now().toString());
+
+            // leemos la plantilla para el reporte.
+            File filetext = new File(FacesContext
+                    .getCurrentInstance()
+                    .getExternalContext()
+                    .getRealPath("/PlantillasReportes/EstadoDeCuenta.jasper"));
+
+            // llenamos la plantilla con los datos.
+            JasperPrint jasperPrint = JasperFillManager.fillReport(
+                    filetext.getPath(),
+                    null,
+                    new JRBeanCollectionDataSource(this.lista_Estado_De_Cuenta)
+            );
+
+            // exportamos a pdf.
+            JasperExportManager.exportReportToPdfStream(jasperPrint, stream);
+            //JasperExportManager.exportReportToXmlStream(jasperPrint, outputStream);
+
+            stream.flush();
+            stream.close();
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            // enviamos la respuesta.
+            fc.responseComplete();
+
+            System.out.println("fin proccess");
+        }
     }
 
     //Getters y Setters de las Listas
@@ -82,22 +111,6 @@ public class Estado_De_CuentaController implements Serializable {
 
     public void setLista_Estado_De_Cuenta_Cliente(List<Estado_De_Cuenta> lista_Estado_De_Cuenta_Cliente) {
         this.lista_Estado_De_Cuenta_Cliente = lista_Estado_De_Cuenta_Cliente;
-    }
-
-    public ExcelOptions getExcelOpt() {
-        return excelOpt;
-    }
-
-    public void setExcelOpt(ExcelOptions excelOpt) {
-        this.excelOpt = excelOpt;
-    }
-
-    public PDFOptions getPdfOpt() {
-        return pdfOpt;
-    }
-
-    public void setPdfOpt(PDFOptions pdfOpt) {
-        this.pdfOpt = pdfOpt;
     }
     
     //Fin
