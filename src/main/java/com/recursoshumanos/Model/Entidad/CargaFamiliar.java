108466ce0a648e55240b59bf547dff5d1cf82abf
kestradalp <kestradalp@gmail.com>
2021-08-30 23:58:56 -0500

    Cambio de paquete de nombres en las entidades
    Se corrigió la dirección de la dirección de las clases: Amonestacion, Asistencia, CargaFamiliar,  Cargo, Ciudad, Dedicacion, Departamento, DetalleHorario, DetalleRolPago, DiaSemana, Empleado, EmpleadoPuesto, EmpleadoReserva, EmpleadoSucursal, Empresa

--- src/main/java/com/recursoshumanos/Model/Entidad/CargaFamiliar.java
+++ src/main/java/com/recursoshumanos/Model/Entidad/CargaFamiliar.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model.Entidad;
+package com.recursoshumanos.Model.Entidad;
 
 import Model.DAO.TipoRubroDAO;
 import java.util.Date;
