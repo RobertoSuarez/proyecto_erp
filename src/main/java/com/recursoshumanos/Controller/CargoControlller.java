bdab2c61c54e2804d29665bfd170fa4e9d37a272
ClasK7 <vacaclaskevin@hotmail.com>
2021-09-11 23:16:06 -0500

    CargoController JD

--- src/main/java/com/recursoshumanos/Controller/CargoControlller.java
+++ src/main/java/com/recursoshumanos/Controller/CargoControlller.java
@@ -19,27 +19,56 @@ import org.primefaces.PrimeFaces;
 import org.primefaces.event.RowEditEvent;
 
 /**
- *
- * @author ClasK7
- */
+  * 
+  * @author kestradalp
+  * @author ClasK7
+  * @author rturr
+  * 
+  * Las clases CONTROLLER son los que responden a la interacción
+  * (eventos del mismo) que hace el usuario en la interfaz
+  * y realiza las peticiones al modelDAO
+  */
 @Named(value = "cargoView")
 @ViewScoped
 public class CargoControlller implements Serializable {
 
+    /**
+     * Se declaran las variables del modelo Controlador de
+     * la parte de Cargo.
+     */
     private Cargo cargo;
     private final CargoDAO cargoDAO;
     private List<Cargo> lista;
 
+    /**
+     * Se crean las nuevas variables para ser 
+     * asignadas.
+     */
     public CargoControlller() {
         cargoDAO = new CargoDAO(new Cargo());
         lista = new ArrayList<>();
     }
 
+    /**
+     * La notación POSTCONSTRUCT define un método como un método
+     * de inicialización de un bean que se ejecuta después de que
+     * se complete el ingreso de la dependencia.
+     */
     @PostConstruct
+    /**
+     * Se crea el constructor
+     */
     public void constructorCargo() {
         lista = cargoDAO.Listar();
     }
-
+    
+     /**
+     * A continuación continuan los métodos de GET y SET
+     * de cada una de las variables declaradas al inicio de
+     * la clase.
+     * @return lista Los GET tienen un return que nos retornan
+     * los datos y los SET una variable que recibe el dato.
+     */
     public List<Cargo> getLista() {
         return lista;
     }
@@ -89,13 +118,23 @@ public class CargoControlller implements Serializable {
         mostrarMensajeInformacion("Se canceló");
     }
 
-    //  MENSAJE DE AVISO
+    /**
+     * Evento que muestra el mensaje de informaciòn en la interfaz
+     * de que ha sido Éxitoso el mensaje 
+     * @param mensaje Objeto que almacena la información
+     * ha ser mostrada en la interfaz.
+     */
     public void mostrarMensajeInformacion(String mensaje) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito", mensaje);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
-    //  MENSAJE DE ERROR
+    /**
+     * Evento que muestra el mensaje de informaciòn en la interfaz
+     * de que ha sido con Error el mensaje 
+     * @param mensaje Objeto que almacena la información
+     * ha ser mostrada en la interfaz.
+     */
     public void mostrarMensajeError(String mensaje) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", mensaje);
         FacesContext.getCurrentInstance().addMessage(null, message);
