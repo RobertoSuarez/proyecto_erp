2f37dea16126441db551db79aed96430d3642f40
ClasK7 <vacaclaskevin@hotmail.com>
2021-09-12 11:54:02 -0500

    Dedicacion_Controller JD

--- src/main/java/com/recursoshumanos/Controller/DedicacionController.java
+++ src/main/java/com/recursoshumanos/Controller/DedicacionController.java
@@ -18,27 +18,55 @@ import javax.inject.Named;
 import org.primefaces.PrimeFaces;
 
 /**
- *
- * @author ClasK7
- */
+  * 
+  * @author kestradalp
+  * @author ClasK7
+  * @author rturr
+  * 
+  * Las clases CONTROLLER son los que responden a la interacción
+  * (eventos del mismo) que hace el usuario en la interfaz
+  * y realiza las peticiones al modelDAO
+  */
 @Named(value = "dedicacionView")
 @ViewScoped
 public class DedicacionController implements Serializable {
 
+    /**
+     * Se declaran las variables del modelo Controlador de
+     * la parte de Dedicación.
+     */
     private Dedicacion dedicacion;
     private final DedicacionDAO dedicacionDAO;
     private List<Dedicacion> lista;
 
+    /**
+     * Se crea las nuevas variables para asignarlos
+     */
     public DedicacionController() {
         dedicacionDAO = new DedicacionDAO(new Dedicacion());
         lista = new ArrayList<>();
     }
 
+    /**
+     * La notación POSTCONSTRUCT define un método como un método
+     * de inicialización de un bean que se ejecuta después de que
+     * se complete el ingreso de la dependencia.
+     */
     @PostConstruct
+    /**
+     * Se crea el constructor
+     */
     public void constructorDedicacion() {
         lista = dedicacionDAO.Listar();
     }
 
+    /**
+     * A continuación continuan los métodos de GET y SET
+     * de cada una de las variables declaradas al inicio de
+     * la clase.
+     * @return lista Los GET tienen un return que nos retornan
+     * los datos y los SET una variable que recibe el dato.
+     */
     public Dedicacion getDedicacion() {
         return dedicacion;
     }
@@ -55,11 +83,18 @@ public class DedicacionController implements Serializable {
         this.lista = dedicaciones;
     }
 
+    /**
+     * evento que permite el abrir o generar una nueva dedicación
+     */
     public void abrirNuevo() {
         this.dedicacion = new Dedicacion();
         PrimeFaces.current().ajax().update("form:messages", "form:manage-dedicacion-content");
     }
 
+    /**
+     * Evento que permite enviar los datos, para ejecutar 
+     * los insert o updates en la BD.
+     */
     public void enviar() {
         dedicacionDAO.setDedicacion(dedicacion);
         if (dedicacion.getId() == 0) {
@@ -80,12 +115,23 @@ public class DedicacionController implements Serializable {
         PrimeFaces.current().ajax().update("form:messages", "form:dt-dedicaciones");
     }
 
+    /**
+     * Evento que muestra el mensaje de informaciòn en la interfaz
+     * de que ha sido Éxitoso el mensaje 
+     * @param mensaje Objeto que almacena la información
+     * ha ser mostrada en la interfaz.
+     */
     public void mostrarMensajeInformacion(String mensaje) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito", mensaje);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
-    //  MENSAJE DE ERROR
+    /**
+     * Evento que muestra el mensaje de informaciòn en la interfaz
+     * de que ha sido con Error el mensaje 
+     * @param mensaje Objeto que almacena la información
+     * ha ser mostrada en la interfaz.
+     */
     public void mostrarMensajeError(String mensaje) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", mensaje);
         FacesContext.getCurrentInstance().addMessage(null, message);
