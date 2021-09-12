7c7d8bb1719e26c0077e758a3c2ff5fb94db67ba
kestradalp <kestradalp@gmail.com>
2021-09-12 12:32:20 -0500

    Update DepartamentoController Java Doc

--- src/main/java/com/recursoshumanos/Controller/DepartamentoControlller.java
+++ src/main/java/com/recursoshumanos/Controller/DepartamentoControlller.java
@@ -20,24 +20,45 @@ import javax.inject.Named;
 import org.primefaces.PrimeFaces;
 
 /**
- *
- * @author rturr
- */
+  * 
+  * @author kestradalp
+  * @author ClasK7
+  * @author rturr
+  * 
+  * Las clases CONTROLLER son los que responden a la interacción
+  * (eventos del mismo) que hace el usuario en la interfaz
+  * y realiza las peticiones al modelDAO
+  */
 @Named(value = "departamentoView")
 @ViewScoped
 public class DepartamentoControlller implements Serializable {
 
+    /**
+     * Se declaran las variables del modelo Controlador de
+     * la parte de Ciudad.
+     */
     private Departamento departamento;
     private final DepartamentoDAO departamentoDAO;
     private List<Departamento> lista;
 
+    /**
+     * Se crea las nuevas variables para asignarlos
+     */
     public DepartamentoControlller() {
         departamento = new Departamento();
         departamentoDAO = new DepartamentoDAO(new Departamento());
         lista = new ArrayList<>();
     }
 
+    /**
+     * La notación POSTCONSTRUCT define un método como un método
+     * de inicialización de un bean que se ejecuta después de que
+     * se complete el ingreso de la dependencia.
+     */
     @PostConstruct
+    /**
+     * Se crea el constructor
+     */
     public void constructorDepartamento() {
         lista = departamentoDAO.Listar();
     }
@@ -68,20 +89,46 @@ public class DepartamentoControlller implements Serializable {
         PrimeFaces.current().ajax().update("form:messages", "form:dt-departamentos");
     }
     
+    /**
+     * Permite habilitar o deshabilitar el estado del departamento
+     * al momento de que el usuario presione el botón del mismo.
+     * @param departamento Objeto que lleva el evento de activado
+     * y desactivado
+     */
     public void cambiarEstado(Departamento departamento){
         departamentoDAO.setDepartamento(departamento);
         departamentoDAO.cambiarEstado();
         PrimeFaces.current().ajax().update("form:messages", "form:dt-puestoLaborals");
     }
     
+    /**
+     * Da formato a las fechas para una clara visualización en 
+     * la interfaz del usuario y asu vez permite registrar
+     * de forma correcta la fecha en la Base de Datos
+     * @param fecha Objeto que carga con el formato de la fecha
+     * @return fecha retorna la fecha con el nuevo parametro
+     */
     public String darFormato(Date fecha){
         return fecha != null? new SimpleDateFormat("dd/MM/yyyy").format(fecha):"";
     }
 
+    /**
+     * Evento que permite abrir un nuevo Departamento
+     * a lo que el usuario presiona el botón de NUEVO en la
+     * interfaz gràfica
+     */
     public void abrirNuevo() {
         this.departamento = new Departamento();
     }
 
+    
+    /**
+     * A continuación continuan los métodos de GET y SET
+     * de cada una de las variables declaradas al inicio de
+     * la clase.
+     * @return lista Los GET tienen un return que nos retornan
+     * los datos y los SET una variable que recibe el dato.
+     */
     public Departamento getDepartamento() {
         return departamento;
     }
@@ -98,13 +145,23 @@ public class DepartamentoControlller implements Serializable {
         this.lista = departamentos;
     }
 
-    //  MENSAJE DE AVISO
+    /**
+     * Evento que muestra el mensaje de informaciòn en la interfaz
+     * de que ha sido Éxitoso el mensaje 
+     * @param mensaje Objeto que almacena la información
+     * ha ser mostrada en la interfaz.
+     */
     public void mostrarMensajeInformacion(String mensaje) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito", mensaje);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
-    //  MENSAJE DE ERROR
+    /**
+     * Evento que muestra el mensaje de informaciòn en la interfaz
+     * de que ha sido con Error el mensaje 
+     * @param mensaje Objeto que almacena la información
+     * ha ser mostrada en la interfaz.
+     */
     public void mostrarMensajeError(String mensaje) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", mensaje);
         FacesContext.getCurrentInstance().addMessage(null, message);
