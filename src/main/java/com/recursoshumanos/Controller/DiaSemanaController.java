ddaa1460b4a8f755bbad913af2e4e88211c23fb3
kestradalp <kestradalp@gmail.com>
2021-09-12 12:44:28 -0500

    Update DiaSemanaControl JavaDoc

--- src/main/java/com/recursoshumanos/Controller/DiaSemanaController.java
+++ src/main/java/com/recursoshumanos/Controller/DiaSemanaController.java
@@ -18,27 +18,55 @@ import javax.inject.Named;
 import org.primefaces.PrimeFaces;
 
 /**
- *
- * @author cturriagos
- */
+  * 
+  * @author kestradalp
+  * @author ClasK7
+  * @author rturr
+  * 
+  * Las clases CONTROLLER son los que responden a la interacción
+  * (eventos del mismo) que hace el usuario en la interfaz
+  * y realiza las peticiones al modelDAO
+  */
 @Named(value = "diaSemanaView")
 @ViewScoped
 public class DiaSemanaController implements Serializable {
+    /**
+     * Se declaran las variables del modelo Controlador de
+     * la parte de Día de la Semana.
+     */
     private DiaSemana diaSemana;
     private List<DiaSemana> lista;
     private final DiaSemanaDAO diaSemanaDAO;
 
+    /**
+     * Se crea las nuevas variables para asignarlos
+     */
     public DiaSemanaController() {
         diaSemanaDAO = new DiaSemanaDAO();
         diaSemana = new DiaSemana();
         lista = new ArrayList<>();
     }
 
+    /**
+     * La notación POSTCONSTRUCT define un método como un método
+     * de inicialización de un bean que se ejecuta después de que
+     * se complete el ingreso de la dependencia.
+     */
     @PostConstruct
+    /**
+     * Se crea el constructor
+     */
     public void constructorDiaSemana() {
         lista = diaSemanaDAO.Listar();
     }
 
+    /**
+     * A continuación continuan los métodos de GET y SET
+     * de cada una de las variables declaradas al inicio de
+     * la clase.
+     * @return lista Los GET tienen un return que nos retornan
+     * los datos y los SET una variable que recibe el dato.
+     */
     public List<DiaSemana> getLista() {
         return lista;
     }
@@ -84,13 +112,23 @@ public class DiaSemanaController implements Serializable {
         PrimeFaces.current().ajax().update("form:messages", "form:dt-diaSemanas");
     }
 
-    //  MENSAJE DE AVISO
+    /**
+     * Evento que muestra el mensaje de informaciòn en la interfaz
+     * de que ha sido Éxitoso el mensaje 
+     * @param mensaje Objeto que almacena la información
+     * ha ser mostrada en la interfaz.
+     */
     public void mostrarMensajeInformacion(String mensaje) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito", mensaje);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
-    //  MENSAJE DE ERROR
+    /**
+     * Evento que muestra el mensaje de informaciòn en la interfaz
+     * de que ha sido con Error el mensaje 
+     * @param mensaje Objeto que almacena la información
+     * ha ser mostrada en la interfaz.
+     */
     public void mostrarMensajeError(String mensaje) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", mensaje);
         FacesContext.getCurrentInstance().addMessage(null, message);
