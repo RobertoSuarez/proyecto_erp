77c584d7f89d2b910c59c0406667d185479af08e
kestradalp <kestradalp@gmail.com>
2021-09-12 13:58:42 -0500

    EmpleadoPuesto_Control JavaDoc

--- src/main/java/com/recursoshumanos/Controller/EmpleadoPuestoController.java
+++ src/main/java/com/recursoshumanos/Controller/EmpleadoPuestoController.java
@@ -27,25 +27,50 @@ import javax.inject.Named;
 import org.primefaces.PrimeFaces;
 
 /**
- *
- * @author cturriagos
- */
+  * 
+  * @author kestradalp
+  * @author ClasK7
+  * @author rturr
+  * 
+  * Las clases CONTROLLER son los que responden a la interacción
+  * (eventos del mismo) que hace el usuario en la interfaz
+  * y realiza las peticiones al modelDAO
+  */
 @Named(value = "empleadoPuestoView")
 @ViewScoped
 public class EmpleadoPuestoController implements Serializable {
     
+    /**
+     * Se declaran las variables del modelo Controlador de
+     * la parte de Empleado Puesto Parte1.
+     */
     private final EmpleadoSucursalDAO empleadoSucursalDAO;
     private final EmpleadoPuestoDAO empleadoPuestoDAO;
     
+    /**
+     * Se declaran las variables del modelo Controlador de
+     * la parte de Empleado Puesto Parte2.
+     */
     private EmpleadoPuesto empleadoPuesto;
     private EmpleadoSucursal empleadoSucursal;
     
+    /**
+     * Se declaran las variables del modelo Controlador de
+     * la parte de Empleado Puesto Parte3.
+     */
     private List<PuestoLaboral> puestos;
     private List<HorarioLaboral> horarios;
     private List<Sucursal> sucursales;
     
+    /**
+     * Se declaran las variables del modelo Controlador de
+     * la parte de Empleado Puesto Parte4.
+     */
     private int idPuesto, idHorario, idSucursal;
 
+    /**
+     * Se crea las nuevas variables para asignarlos
+     */
     public EmpleadoPuestoController() {
         empleadoSucursalDAO = new EmpleadoSucursalDAO();
         empleadoPuestoDAO = new EmpleadoPuestoDAO();
@@ -56,7 +81,15 @@ public class EmpleadoPuestoController implements Serializable {
         sucursales = new ArrayList<>();
     }
 
+    /**
+     * La notación POSTCONSTRUCT define un método como un método
+     * de inicialización de un bean que se ejecuta después de que
+     * se complete el ingreso de la dependencia.
+     */
     @PostConstruct
+    /**
+     * Se crea el constructor
+     */
     public void constructorEmpleadoPuestoEditar() {
         PuestoLaboralDAO puestoLaboralDAO = new PuestoLaboralDAO();
         puestos = puestoLaboralDAO.Activos();
@@ -67,6 +100,13 @@ public class EmpleadoPuestoController implements Serializable {
         
     }
 
+    /**
+     * A continuación continuan los métodos de GET y SET
+     * de cada una de las variables declaradas al inicio de
+     * la clase.
+     * @return lista Los GET tienen un return que nos retornan
+     * los datos y los SET una variable que recibe el dato.
+     */
     public EmpleadoPuesto getEmpleadoPuesto() {
         return empleadoPuesto;
     }
@@ -131,6 +171,10 @@ public class EmpleadoPuestoController implements Serializable {
         this.idSucursal = idSucursal;
     }
     
+    /**
+     * Evento que carga al empleado sucursal
+     * @param idEmpleado Objeto que carga el ID del empleado
+     */
     public void postLoad(int idEmpleado) {
         if (idEmpleado > 0 ) {
             EmpleadoDAO empleadoDAO = new EmpleadoDAO();
@@ -144,6 +188,11 @@ public class EmpleadoPuestoController implements Serializable {
         }
     }
 
+    /**
+     * Eventio que regustra al nuevo empleado al presioanr el
+     * botón de guardar en la interfaz del usuario
+     *
+     */
     public void guardar() {
         if (empleadoPuesto.getPuestoLaboral().getId() != idPuesto || empleadoPuesto.getHorarioLaboral().getId() != idHorario) {
             empleadoPuestoDAO.desactivar();
@@ -183,12 +232,23 @@ public class EmpleadoPuestoController implements Serializable {
         PrimeFaces.current().ajax().update("form:messages", "form:dt-puesto");
     }
 
-    //  MENSAJE DE ERROR
+    /**
+     * Evento que muestra el mensaje de informaciòn en la interfaz
+     * de que ha sido con Error el mensaje 
+     * @param mensaje Objeto que almacena la información
+     * ha ser mostrada en la interfaz.
+     */
     public void mostrarMensajeError(String mensaje) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", mensaje);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
+    /**
+     * Evento que muestra el mensaje de informaciòn en la interfaz
+     * de que ha sido Éxitoso el mensaje 
+     * @param mensaje Objeto que almacena la información
+     * ha ser mostrada en la interfaz.
+     */
     public void mostrarMensajeInformacion(String mensaje) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito", mensaje);
         FacesContext.getCurrentInstance().addMessage(null, message);
