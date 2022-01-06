88ac3b6c478276c514c36670c8302f1b9dd1a71c
eguarangao <69706207+eguarangao@users.noreply.github.com>
2022-01-06 01:48:06 -0500

    Actualizando dao y controladores
    Aun requiere la implementacion en el login el registrar se a culminado

--- src/main/java/com/registroUsuario/controllers/UsuarioController.java
+++ src/main/java/com/registroUsuario/controllers/UsuarioController.java
@@ -7,6 +7,8 @@ package com.registroUsuario.controllers;
 import com.registroUsuario.dao.UsuarioDAO;
 import com.registroUsuario.models.Usuario;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.faces.application.FacesMessage;
@@ -17,77 +19,95 @@ import javax.inject.Named;
 @Named(value = "usuarioMB")
 @ViewScoped
 public class UsuarioController implements Serializable {
-
+    
     Usuario usuario;
+    private final List<Usuario> listaUsuario;
     UsuarioDAO usuarioDAO;
     String warnMsj = "Advertencia";
     String infMsj = "Exito";
-
+    
     public UsuarioController() {
         usuario = new Usuario();
         usuarioDAO = new UsuarioDAO();
-
+        listaUsuario = new ArrayList<>();
     }
-
+    
     public Usuario getUsuario() {
         return usuario;
     }
-
+    
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-
+    
     public UsuarioDAO getUsuarioDAO() {
         return usuarioDAO;
     }
-
+    
     public void setUsuarioDAO(UsuarioDAO usuarioDAO) {
         this.usuarioDAO = usuarioDAO;
     }
-
-    public void registrarUsuario() {
+    
+    public void registrarUsuario() throws Exception {
+        
         Pattern pattern = Pattern
                 .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
                         + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-        //  Matcher matcher = pattern.matcher(usuario.getEmail())  ///Ingresar dato Email a la BD
-        //  this.usuarioDAO.registrarUsuario(usuario);
+        Matcher matcher = pattern.matcher(usuario.getEmail());  ///Ingresar dato Email a la BD
+        //  
         try {
-
+            
             if ("".equals(usuario.getNombre())) {
-                PFW("Ingrese un Nombre");               
+                PFW("Ingrese un Nombre");
 
                 //  PrimeFaces.current().ajax().update("form:messages");
             } else if ("".equals(usuario.getApellido())) {
                 PFW("Ingrese un Apellido");
-
+                
             } else if ("".equals(usuario.getUsername())) {
                 PFW("Ingrese un Usuario");
-
+                
             } else if ("".equals(usuario.getPassword())) {
                 PFW("Ingrese una Contraseña");
-
+                
+            } else if (matcher.find() == false) {
+                PFW("Ingrese un email válido");
             } else {
+                this.usuarioDAO.registrarUsuario(usuario);
                 PFE("Usuario registrado");
-
+                
             }
         } catch (Exception e) {
+            
+        }
+        
+    }
+    
+    public void iniciarSesion() {
+
+        /*listaUsuario = new ArrayList<>();
+
+        listaUsuario = usuarioDAO.iniciarSesion(usuario);
+
+        for (int x = 0; x < listaUsuario.size(); x++) {
+            System.out.println("- " + listaUsuario.get(x).getMsj().toString() + " Ebert Gua");
 
         }
-
+         */
     }
-
+    
     public void PFW(String msj) {
         FacesContext.getCurrentInstance().
                 addMessage(null, new FacesMessage(
-         FacesMessage.SEVERITY_WARN, warnMsj, msj));
-
+                        FacesMessage.SEVERITY_WARN, warnMsj, msj));
+        
     }
-
+    
     public void PFE(String msj) {
         FacesContext.getCurrentInstance().
                 addMessage(null, new FacesMessage(
-         FacesMessage.SEVERITY_INFO, infMsj, msj));
-
+                        FacesMessage.SEVERITY_INFO, infMsj, msj));
+        
     }
-
+    
 }
--- src/main/java/com/registroUsuario/dao/UsuarioDAO.java
+++ src/main/java/com/registroUsuario/dao/UsuarioDAO.java
@@ -2,35 +2,110 @@ package com.registroUsuario.dao;
 
 import com.global.config.Conexion;
 import com.registroUsuario.models.Usuario;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UsuarioDAO {
-
+    
     List<Usuario> lsitaUsuarios;
     boolean isTrue = true;
+    Usuario usuario = new Usuario();
     Conexion conexion = new Conexion();
-   
-
+    
     public void registrarUsuario(Usuario user) {
-
+        
         try {
             this.conexion.Conectar();
-            String sentenciaRegtsUser = "INSERT INTO public.usuario(\n"
-                    + "	\"idUsuario\", nombre, apellido, username, password, \"fechaCreacion\", habilitado)\n"
-                    + "	VALUES ('" + user.getIdUsuario() + "','" + user.getNombre()
-                    + "','" + user.getApellido() + "','" + user.getUsername() + "','" + user.getPassword()
-                    + "','" + user.getFehcaCreacion() + "','" + user.isHabilitado() + "');";
-            conexion.Ejecutar2(sentenciaRegtsUser);
+            
+            String sentencia = "SELECT public.registrarusuario('" + user.getNombre() + "',"
+                    + "'" + user.getApellido() + "','" + user.getEmail() + "',"
+                    + "'" + user.getUsername() + "','" + user.getPassword() + "')";
+            conexion.Ejecutar2(sentencia);
             conexion.cerrarConexion();
-
+            
         } catch (SQLException e) {
             e.toString();
         } finally {
+            
+            conexion.cerrarConexion();
+            
+        }
+        
+    }
+    /*
 
+    public void iniciarSesionS(String usuario, String password) {
+        String sentencia = "";
+        try {
+            this.conexion.Conectar();
+
+            sentencia = "SELECT public.iniciarsesion"
+                    + "('" + usuario + "',"
+                    + "'" + password + "')";
+            System.out.println(usuario + "l:j" + password);
+            conexion.Ejecutar2(sentencia);
             conexion.cerrarConexion();
 
-        }
+            System.out.print(sentencia);
+        } catch (SQLException e) {
+            e.toString();
+            System.out.print(sentencia);
+        } finally {
 
+            conexion.cerrarConexion();
+        }
+    }
+/*/
+    ResultSet result;
+    // RETURNS TABLE(code integer, reslt character varying, iduser integer, name character varying, surname character varying, usrnm character varying, pssword character varying, mail character varying) 
+
+    public List<Usuario> iniciarSesion(Usuario u) {
+        lsitaUsuarios = new ArrayList<>();
+        
+        String sentencia = "";
+        if (conexion.isEstado()) {
+            try {
+                sentencia = "SELECT public.iniciarsesion"
+                        + "('" + u.getUsername() + "',"
+                        + "'" + u.getPassword() + "')";
+                result = conexion.ejecutarConsulta(sentencia);
+                
+                while (result.next()) {
+                    
+                    lsitaUsuarios.add(new Usuario(result.getInt(1),
+                            result.getString(2),
+                            result.getInt(3),
+                            result.getString(4),
+                            result.getString(5),
+                            result.getString(6),
+                            result.getString(7),
+                            result.getString(8)
+                    ));
+
+                    // System.out.print(lsitaUsuarios.toString());
+                }
+                
+            } catch (SQLException e) {
+                lsitaUsuarios.toString();
+                for (int x = 0; x < lsitaUsuarios.size(); x++) {
+                    System.out.print(lsitaUsuarios.get(x) + "Guaranga");
+                }
+                
+                System.out.println(e.toString() + "EBERT");
+                return lsitaUsuarios;
+            } finally {
+                
+                conexion.cerrarConexion();
+            }
+        }
+        return lsitaUsuarios;
+    }
+    
+    public void verifica(String user, String pass) {
+        lsitaUsuarios = new ArrayList<>();
+        lsitaUsuarios = iniciarSesion(usuario);
+        lsitaUsuarios.get(2).toString();
     }
 }
