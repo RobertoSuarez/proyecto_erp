1ff95f85aa567de2c7cb164c7d5beb7b3c5327d2
eguarangao <69706207+eguarangao@users.noreply.github.com>
2022-01-09 21:34:18 -0500

    navegacion explicita arrglado

--- src/main/java/com/registroUsuario/controllers/UsuarioController.java
+++ src/main/java/com/registroUsuario/controllers/UsuarioController.java
@@ -6,6 +6,7 @@ package com.registroUsuario.controllers;
 
 import com.registroUsuario.dao.UsuarioDAO;
 import com.registroUsuario.models.Usuario;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,81 +17,91 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 //import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.primefaces.PrimeFaces;
 
 @Named(value = "usuarioMB")
 @SessionScoped
 public class UsuarioController implements Serializable {
-    
+
     Usuario usuario;
     private List<Usuario> listaUsuario;
     UsuarioDAO usuarioDAO;
     String warnMsj = "Advertencia";
     String infMsj = "Exito";
-    private FacesContext facesContext = FacesContext.getCurrentInstance();
+    private final FacesContext facesContext = FacesContext.getCurrentInstance();
     HttpSession httpSession = (HttpSession) facesContext.getExternalContext().getSession(true);
-    
+
     public UsuarioController() {
         usuario = new Usuario();
         usuarioDAO = new UsuarioDAO();
         listaUsuario = new ArrayList<>();
     }
-    
+
     public Usuario getUsuario() {
         return usuario;
     }
-    
+
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-    
+
     public UsuarioDAO getUsuarioDAO() {
         return usuarioDAO;
     }
-    
+
     public void setUsuarioDAO(UsuarioDAO usuarioDAO) {
         this.usuarioDAO = usuarioDAO;
     }
-    
+
     public void registrarUsuario() throws Exception {
-        
+
         Pattern pattern = Pattern
                 .compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
                         + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
         Matcher matcher = pattern.matcher(usuario.getEmail());  ///Ingresar dato Email a la BD
         //  
         try {
-            
+
             if ("".equals(usuario.getNombre())) {
                 PFW("Ingrese un Nombre");
 
                 //  PrimeFaces.current().ajax().update("form:messages");
             } else if ("".equals(usuario.getApellido())) {
                 PFW("Ingrese un Apellido");
-                
+
             } else if ("".equals(usuario.getUsername())) {
                 PFW("Ingrese un Usuario");
-                
+
             } else if ("".equals(usuario.getPassword())) {
                 PFW("Ingrese una Contraseña");
             } else if (usuario.isHabilitado() == false) {
                 PFW("Aceptar los terminos y condiciones");
-                
+
             } else if (matcher.find() == false) {
                 PFW("Ingrese un email válido");
             } else {
                 this.usuarioDAO.registrarUsuario(usuario);
                 PFE("Usuario registrado");
-                
+
             }
         } catch (Exception e) {
-            
+
         }
-        
+
     }
-    
+
     public String iniciarSesion() {
         listaUsuario = new ArrayList<>();
+        if ("".equals(usuario.getUsername())) {
+            PFW("Ingrese un usuario");
+        }
+        if ("".equals(usuario.getPassword())) {
+            PFW("Ingrese una contraseña");
+        }
         if (!usuario.getUsername().isEmpty() && !usuario.getPassword().isEmpty()) {
             listaUsuario = usuarioDAO.iniciarSesion(usuario);
             if (listaUsuario.get(0).getCode() == -1) {
@@ -99,42 +110,31 @@ public class UsuarioController implements Serializable {
             } else if (listaUsuario.get(0).getCode() == -2) {
                 PFE(listaUsuario.get(0).getMsj());
                 return "";
-                
+
             } else {
                 PFE(listaUsuario.get(0).getMsj());
-                httpSession.setAttribute("usuario", listaUsuario.get(0).getUsername());
-                return "/View/Global/Main.xhtml";
+                httpSession.setAttribute("username", listaUsuario);
+                return "/View/Global/Main?faces-redirect=true";
             }
-            
-        } else if ("".equals(usuario.getUsername())) {
-            PFW("Ingrese un usuario");
-        }else if ("".equals(usuario.getPassword())) {
-            PFW("Ingrese una contraseña");
+
         } else {
             return "";
         }
-        return null;
     }
-    
+
     public void PFW(String msj) {
         FacesContext.getCurrentInstance().
                 addMessage(null, new FacesMessage(
                         FacesMessage.SEVERITY_WARN, warnMsj, msj));
-        
+
     }
-    
+
     public void PFE(String msj) {
         FacesContext.getCurrentInstance().
                 addMessage(null, new FacesMessage(
                         FacesMessage.SEVERITY_INFO, infMsj, msj));
-        
+
     }
-    
-    public String logOutUser() {
-        httpSession.removeAttribute("usuario");
-        usuarioDAO = new UsuarioDAO();
-        usuario = new Usuario();
-        return "index.xhtml";
-    }
-    
+
+
 }
