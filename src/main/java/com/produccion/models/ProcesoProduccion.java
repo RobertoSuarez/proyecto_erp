de1c914fee74ca9d85a337201590861d5732cc9f
AlexHida <alexhidalgocarrera@gmail.com>
2022-01-19 19:12:22 -0500

    Create ProcesoProduccion.java
    Controlador

---
+++ src/main/java/com/produccion/models/ProcesoProduccion.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.produccion.models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Alex
+ */
+public class ProcesoProduccion {
+    private String nombre;
+    private String descripcion;
+    private Date fechaCreacion;
+    private Date fechaModificacion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaModificacion() {
+        return fechaModificacion;
+    }
+
+    public void setFechaModificacion(Date fechaModificacion) {
+        this.fechaModificacion = fechaModificacion;
+    }
+    
+    
+}
