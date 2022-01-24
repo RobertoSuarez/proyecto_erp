2c66baf499c8868ead2c89a4ad9d2e1c036bd6dd
AlexHida <alexhidalgocarrera@gmail.com>
2022-01-24 11:16:14 -0500

    Procesos
    Procesos

--- src/main/java/com/produccion/controllers/ProcesosManagedBean.java
+++ src/main/java/com/produccion/controllers/ProcesosManagedBean.java
@@ -5,9 +5,17 @@
  */
 package com.produccion.controllers;
 
+import com.produccion.dao.ProcesoProduccionDAO;
+import com.produccion.models.ProcesoProduccion;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
+import org.primefaces.PrimeFaces;
 
 /**
  *
@@ -15,5 +23,111 @@ import javax.faces.view.ViewScoped;
  */
 @ManagedBean(name = "procesoMB")
 @ViewScoped
-public class ProcesosManagedBean implements Serializable{
+public class ProcesosManagedBean implements Serializable {
+
+    private List<ProcesoProduccion> listaProcesos = new ArrayList<>();
+    private ProcesoProduccionDAO procesoProduccionDAO = new ProcesoProduccionDAO();
+    private ProcesoProduccion procesoProduccion = new ProcesoProduccion();
+    private ProcesoProduccion selectProceso;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("PostConstruct");
+        listaProcesos = procesoProduccionDAO.getProcesosProduccion();
+    }
+
+    public void closeDialogModal() {
+        PrimeFaces.current().executeScript("PF('procesoPrincDialog').hide()");
+    }
+
+    public void aleatorioIdenti() {
+        String uuid = java.util.UUID.randomUUID().toString().substring(4, 7).toUpperCase();
+        String uuid2 = java.util.UUID.randomUUID().toString().substring(4, 7);
+        this.procesoProduccion.setIdentificador("PR-" + uuid + uuid2);
+    }
+
+    public void openNew() {
+        this.selectProceso = new ProcesoProduccion();
+        aleatorioIdenti();
+    }
+
+    public void insertar() {
+        try {
+            if ("".equals(procesoProduccion.getIdentificador())) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Advertencia", "Ingrese un Identificador"));
+            } else if ("".equals(procesoProduccion.getNombre())) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Advertencia", "Ingrese un Nombre"));
+            } else if ("".equals(procesoProduccion.getDescripcion())) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Advertencia", "Ingrese una Descripción"));
+            } else {
+                this.procesoProduccionDAO.insertarp(procesoProduccion);
+                FacesContext.getCurrentInstance().
+                        addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Exito", "Proceso Agregado"));
+            }
+
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().
+                    addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "",
+                            "Error al guardar"));
+        }
+        closeDialogModal();
+        PrimeFaces.current().ajax().update("form-princ:dtProcesoPrin");
+    }
+
+    public void editar() {
+        try {
+            if ("".equals(procesoProduccion.getIdentificador())) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Advertencia", "Ingrese un Codigo"));
+            } else if ("".equals(procesoProduccion.getNombre())) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Advertencia", "Ingrese un Nombre"));
+            } else if ("".equals(procesoProduccion.getDescripcion())) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Advertencia", "Ingrese una Descripción"));
+            } else {
+                this.procesoProduccionDAO.update(procesoProduccion, procesoProduccion.getCodigo_proceso());
+                FacesContext.getCurrentInstance().
+                        addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Exito", "Proceso Guardado"));
+            }
+
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().
+                    addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "",
+                            "Error al guardar"));
+        }
+        PrimeFaces.current().executeScript("PF('procesoPrincDialog').hide()");
+        PrimeFaces.current().ajax().update("form:dtProcesoPrin", "form:growl");
+    }
+
+
+    public ProcesoProduccion getProcesoProduccion() {
+        return procesoProduccion;
+    }
+
+    public void setProcesoProduccion(ProcesoProduccion procesoProduccion) {
+        this.procesoProduccion = procesoProduccion;
+    }
+
+    public List<ProcesoProduccion> getListaProcesos() {
+        return listaProcesos;
+    }
+
+    public void setListaProcesos(List<ProcesoProduccion> listaProcesos) {
+        this.listaProcesos = listaProcesos;
+    }
+
+    public ProcesoProduccionDAO getProcesoProduccionDAO() {
+        return procesoProduccionDAO;
+    }
+
+    public void setProcesoProduccionDAO(ProcesoProduccionDAO procesoProduccionDAO) {
+        this.procesoProduccionDAO = procesoProduccionDAO;
+    }
+
+    public ProcesoProduccion getSelectProceso() {
+        return selectProceso;
+    }
+
+    public void setSelectProceso(ProcesoProduccion selectProceso) {
+        this.selectProceso = selectProceso;
+    }
+
 }
--- src/main/java/com/produccion/dao/ProcesoProduccionDAO.java
+++ src/main/java/com/produccion/dao/ProcesoProduccionDAO.java
@@ -5,10 +5,83 @@
  */
 package com.produccion.dao;
 
+import com.global.config.Conexion;
+import com.produccion.models.ProcesoProduccion;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Alex
  */
 public class ProcesoProduccionDAO {
+
+    private Conexion conexion;
+    private ResultSet resultSet;
+
+    public ProcesoProduccionDAO() {
+        conexion = new Conexion();
+    }
     
+    public List<ProcesoProduccion> getProcesosProduccion() {
+        List<ProcesoProduccion> procesos = new ArrayList<>();
+        String sql = String.format("select * from getProcesosProduccion();");
+        try {
+            conexion.conectar();
+            resultSet = conexion.ejecutarSql(sql);
+            //Llena la lista de los datos
+            while (resultSet.next()) {
+                procesos.add(new ProcesoProduccion(resultSet.getInt("codigo_proceso"), resultSet.getString("nombre"),
+                        resultSet.getString("descripcion"), resultSet.getString("identificador")));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            conexion.desconectar();
+        }
+        return procesos;
+    }
+
+    public void insertarp(ProcesoProduccion proceso) {
+        try {
+            this.conexion.Conectar();
+            String sql = "INSERT INTO public.proceso_produccion(\n"
+                    + "	nombre, descripcion, identificador\n"
+                    + "	VALUES ('" + proceso.getNombre() + "','"+proceso.getDescripcion()+ "'"
+                    + ",'" + proceso.getIdentificador()+ "');";
+            
+            String sql2="INSERT INTO public.proceso_produccion(nombre, descripcion, identificador)\n" +
+"	VALUES ('" + proceso.getNombre() + "', '"+proceso.getDescripcion()+ "', '" + proceso.getIdentificador()+ "')";
+
+            conexion.Ejecutar2(sql2);
+            conexion.cerrarConexion();
+
+        } catch (Exception e) {
+        } finally {
+            conexion.cerrarConexion();
+        }
+    }
+
+    public void update(ProcesoProduccion proceso, int codigo) throws SQLException {
+        try {
+            this.conexion.Conectar();
+            String sql = "UPDATE public.proceso_produccion\n"
+            +" SET nombre='"+proceso.getNombre()+"',"
+            +" descripcion='"+proceso.getNombre()+"', "
+            +" identificador='"+proceso.getNombre()+"', "
+            +"Where codigo_proceso= "+codigo+"";
+            
+            conexion.ejecutar(sql);
+            conexion.cerrarConexion();
+            
+        } catch (SQLException e) {
+               throw e;
+          } finally {
+               this.conexion.cerrarConexion();
+          }
+    }
+
 }
