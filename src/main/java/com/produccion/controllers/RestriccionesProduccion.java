39e173a8855e60c79eb4e5a3b81ea44cbfee6764
Derian2426 <victor.chun2017@uteq.edu.ec>
2022-01-26 15:13:29 -0500

    Rproduccion

---
+++ src/main/java/com/produccion/controllers/RestriccionesProduccion.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.produccion.controllers;
+
+import com.seguridad.dao.UsuarioDAO;
+import com.seguridad.models.Roles;
+import java.io.IOException;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import org.primefaces.PrimeFaces;
+
+/**
+ *
+ * @author HP
+ */
+@Named(value = "restriccionesProduccion")
+@ViewScoped
+public class RestriccionesProduccion implements Serializable {
+
+    private UsuarioDAO usuarioDAO;
+    FacesContext context = FacesContext.getCurrentInstance();
+    List<Roles> listaRoles = (List<Roles>) context.getExternalContext().getSessionMap().get("usuario_rol");
+    ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+
+    public RestriccionesProduccion() {
+        usuarioDAO = new UsuarioDAO();
+    }
+
+    public void restriccionUser() {
+        try {
+            if (!"Jefe de Producción".equals(listaRoles.get(0).getNombre())
+                    || !"Operario".equals(listaRoles.get(0).getNombre())||
+                    !"Gerente".equals(listaRoles.get(0).getNombre())) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage
+        (FacesMessage.SEVERITY_INFO, "Aviso", "Solo los Administradores y personal "
+                        + "encargado de producción pueden ingresar al módulo."));
+            } else {
+//                FacesContext.getCurrentInstance().getExternalContext().redirect
+//        ("../produccion/listaOrdenProduccion.xhtml");
+                externalContext.redirect("../produccion/listaOrdenProduccion.xhtml");
+            }
+        } catch (IOException e) {
+            
+        }
+
+        System.out.println("Holaaaaa" + listaRoles.get(0).getNombre());
+    }
+    public void redireccion() throws IOException {
+        
+        if (!"Gerente".equals(listaRoles.get(0).getNombre()) || 
+                !"Jefe de Producción".equals(listaRoles.get(0).getNombre())|| 
+                !"Operario".equals(listaRoles.get(0).getNombre())){
+                externalContext.redirect("/proyecto_erp/View/Global/Main.xhtml");}
+    }
+
+}
