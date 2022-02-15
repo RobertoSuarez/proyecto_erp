e4a6d59621b504d14bc11cd20f09aed4d4ac2a57
neoshock <pideun@gmail.com>
2022-02-14 23:14:17 -0500

    Merge origin/main into main
    Conflicts:
    	src/main/java/com/contabilidad/dao/MovimientoDAO.java
    	src/main/webapp/View/activos_fijos/ActivosIntangibles.xhtml
    	src/main/webapp/View/activos_fijos/ActivosTangibles.xhtml

--- src/main/java/com/seguridad/controllers/RolController.java
+++ src/main/java/com/seguridad/controllers/RolController.java
@@ -5,10 +5,38 @@
  */
 package com.seguridad.controllers;
 
+import com.seguridad.dao.UsuarioDAO;
+import com.seguridad.models.Roles;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
 /**
  *
  * @author Andres Mora
  */
-public class RolController {
-    
+@Named
+@ViewScoped
+public class RolController implements Serializable {
+
+    private UsuarioDAO usuarioDAO;
+    FacesContext context = FacesContext.getCurrentInstance();
+    ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+    List<Roles> listaRoles = (List<Roles>) context.getExternalContext().getSessionMap().get("usuario_rol");
+
+    public RolController() {
+        usuarioDAO = new UsuarioDAO();
+    }
+
+    public String renderPrincipalView() {
+        if (listaRoles.get(0).getNombre().equals("Gerente")) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+
 }
