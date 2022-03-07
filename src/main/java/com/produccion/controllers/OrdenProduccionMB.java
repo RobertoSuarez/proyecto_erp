cbfd256073541f6f5a9857f4ddafea58a1400a71
Derian2426 <victor.chun2017@uteq.edu.ec>
2022-03-07 08:35:48 -0500

    reporte

--- src/main/java/com/produccion/controllers/OrdenProduccionMB.java
+++ src/main/java/com/produccion/controllers/OrdenProduccionMB.java
@@ -5,17 +5,30 @@
  */
 package com.produccion.controllers;
 
+import com.empresa.global.EmpresaMatrizDAO;
 import com.produccion.dao.OrdenProduccionDAO;
 import com.produccion.models.ArticuloFormula;
 import com.produccion.models.FormulaProduccion;
 import com.produccion.models.OrdenProduccion;
 import com.produccion.models.OrdenTrabajo;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.annotation.PostConstruct;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 /**
  *
@@ -39,6 +52,7 @@ public class OrdenProduccionMB implements Serializable {
     private List<ArticuloFormula> listaMateriaPrima;
     List<ArticuloFormula> listaMateriaPrimaAdicional;
     private List<ArticuloFormula> listaMateriaPrimaTotal;
+    private String empresa = EmpresaMatrizDAO.getEmpresa().getNombre();
 
     public OrdenProduccionMB() {
         ordenTerminada = new OrdenTrabajo();
@@ -200,6 +214,63 @@ public class OrdenProduccionMB implements Serializable {
             materiales.setTotal(materiales.getCantidad() * materiales.getCosto());
             listaMateriaPrimaTotal.add(materiales);
         }
+    }
+
+    public void exportpdf() throws IOException, JRException {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        ExternalContext ec = fc.getExternalContext();
+
+        // Cabecera de la respuesta.
+        ec.responseReset();
+        ec.setResponseContentType("application/pdf");
+        ec.setResponseHeader("Content-disposition", String.format("attachment; filename=OrdenProduccion.pdf"));
+
+        // tomamos el stream para llenarlo con el pdf.
+        try (OutputStream stream = ec.getResponseOutputStream()) {
+
+            // Parametros para el reporte.
+            Map<String, Object> parametros = new HashMap<String, Object>();
+            parametros.put("nombreempresa", empresa);
+            parametros.put("cantidad", ordenTerminada.getCantidad());
+            parametros.put("costoTotal", ordenTerminada.getCostoTotal());
+            parametros.put("totalMateria", ordenTerminada.getTotalMateria());
+            parametros.put("totalMOD", ordenTerminada.getTotalMOD());
+            parametros.put("totalCIF", ordenTerminada.getTotalCIF());
+
+            // leemos la plantilla para el reporte.
+            File filetext = new File(FacesContext
+                    .getCurrentInstance()
+                    .getExternalContext()
+                    .getRealPath("/PlantillasReportes/produccionReporte.jasper"));
+
+            // llenamos la plantilla con los datos.
+            JasperPrint jasperPrint = JasperFillManager.fillReport(
+                    filetext.getPath(),
+                    parametros,
+                    new JRBeanCollectionDataSource(this.listaMateriaPrimaTotal)
+            );
+
+            // exportamos a pdf.
+            JasperExportManager.exportReportToPdfStream(jasperPrint, stream);
+            //JasperExportManager.exportReportToXmlStream(jasperPrint, outputStream);
+
+            stream.flush();
+            stream.close();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            // enviamos la respuesta.
+            fc.responseComplete();
+        }
+
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
     }
 
 }
